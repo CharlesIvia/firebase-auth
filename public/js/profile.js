f06d0b6097531ec50b4f5c0df0827272558cad88
Charles Ivia <iviacharles@gmail.com>
2020-07-03 16:58:20 +0300

    Polished redirect links across the app for smooth navigation

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,28 +1,32 @@
-const logOut = document.getElementById('logOut');
-const mergeAccounts = document.getElementById('mergeAccounts');
-const modifyAccount = document.getElementById('modifyAccount');
-const displayNameHolder = document.getElementById('displayNameHolder');
-const photoHolder = document.getElementById('photoHolder');
+const logOut = document.getElementById("logOut");
+const mergeAccounts = document.getElementById("mergeAccounts");
+const modifyAccount = document.getElementById("modifyAccount");
+const displayNameHolder = document.getElementById("displayNameHolder");
+const photoHolder = document.getElementById("photoHolder");
 
 const auth = firebase.auth();
 
-logOut.addEventListener('click', () => {
-    //signOut() is a built in firebase function responsible for signing a user out
-    auth.signOut()
+logOut.addEventListener("click", () => {
+  //signOut() is a built in firebase function responsible for signing a user out
+  auth
+    .signOut()
     .then(() => {
-        window.location.assign('../');
+      window.location.href = "index.html";
+      //   window.location.assign("../");
     })
-    .catch(error => {
-        console.error(error);
-    })
-})
+    .catch((error) => {
+      console.error(error);
+    });
+});
 
 //Go to modification page
-modifyAccount.addEventListener('click', () => {
-    window.location.assign('../edit');
+modifyAccount.addEventListener("click", () => {
+  location.href = "edit.html";
+  //   window.location.assign("../edit");
 });
 
 //Go to merge accounts page
-mergeAccounts.addEventListener('click', () => {
-    window.location.assign('../merge');
+mergeAccounts.addEventListener("click", () => {
+  location.href = "merge.html";
+  //   window.location.assign("../merge");
 });
