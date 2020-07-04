f14da097072b30fa09b7d31ba8e3dd660425ab75
Charles Ivia <iviacharles@gmail.com>
2020-07-04 15:58:54 +0300

    Created universal information edit logic

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,43 +1,86 @@
-const mailField = document.getElementById('mail');
-const passwordField = document.getElementById('password');
-const displayNameField = document.getElementById('displayName');
-const photoField = document.getElementById('photo');
-const labels = document.getElementsByTagName('label');
-const editButton = document.getElementById('edit');
-const deleteButton = document.getElementById('delete');
+const mailField = document.getElementById("mail");
+const passwordField = document.getElementById("password");
+const displayNameField = document.getElementById("displayName");
+const photoField = document.getElementById("photo");
+const labels = document.getElementsByTagName("label");
+const editButton = document.getElementById("edit");
+const deleteButton = document.getElementById("delete");
 
-mailField.addEventListener('focus', () => {
-    labels.item(0).className = "focused-field";
+const auth = firebase.auth();
+
+// To print your current user information so you can the changes once done
+
+auth.onAuthStateChanged((user) => {
+  console.log(user);
 });
 
-passwordField.addEventListener('focus', () => {
-    labels.item(1).className = "focused-field";
+//Edit function
+
+const editInformation = () => {
+  const newNameAndPhoto = {
+    newDisplayName: displayNameField.value,
+    newPhotoURL: photoField.value,
+  };
+
+  const newEmail = mailField.value;
+  const newPassword = passwordField.value;
+
+  //Holds all information abt ccurrent signed in user
+
+  const user = auth.currentUser;
+  changeNameAndPhoto(user, newNameAndPhoto);
+
+  //Change email and password
+
+  if (newPassword && newEmail) {
+    const credential = createCredential(user);
+    changePassowrd(user, credential, newPassword);
+    changeEmail(user, credential, newEmail);
+  }
+
+  //Change only the email
+  else if (newPassword) {
+    const credential = createCredential(user);
+    changePassowrd(user, credential, newPassword);
+  }
+
+  //change email only
+  else if (newEmail) {
+    const credential = createCredential(user);
+    changeEmail(user, credential, newEmail);
+  }
+};
+
+//Animations
+
+mailField.addEventListener("focus", () => {
+  labels.item(0).className = "focused-field";
 });
 
-mailField.addEventListener('blur', () => {
-    if(!mailField.value)
-        labels.item(0).className = "unfocused-field";
+passwordField.addEventListener("focus", () => {
+  labels.item(1).className = "focused-field";
 });
 
-passwordField.addEventListener('blur', () => {
-    if(!passwordField.value)
-        labels.item(1).className = "unfocused-field";
+mailField.addEventListener("blur", () => {
+  if (!mailField.value) labels.item(0).className = "unfocused-field";
 });
 
-displayNameField.addEventListener('focus', () => {
-    labels.item(2).className = "focused-field";
+passwordField.addEventListener("blur", () => {
+  if (!passwordField.value) labels.item(1).className = "unfocused-field";
 });
 
-photoField.addEventListener('focus', () => {
-    labels.item(3).className = "focused-field";
+displayNameField.addEventListener("focus", () => {
+  labels.item(2).className = "focused-field";
 });
 
-displayNameField.addEventListener('blur', () => {
-    if(!displayNameField.value)
-        labels.item(2).className = "unfocused-field";
+photoField.addEventListener("focus", () => {
+  labels.item(3).className = "focused-field";
 });
 
-photoField.addEventListener('blur', () => {
-    if(!photoField.value)
-        labels.item(3).className = "unfocused-field";
+displayNameField.addEventListener("blur", () => {
+  if (!displayNameField.value) labels.item(2).className = "unfocused-field";
+});
+
+photoField.addEventListener("blur", () => {
+  if (!photoField.value) labels.item(3).className = "unfocused-field";
 });
